1bab66dab8bf022c2e1ca285fc8415e155f57852
Asija Watson <asija.w@yahoo.com>
2019-12-30 02:18:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,19 @@
-class Person
+class Person 
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+  
+  def job
+    @job
+  end
+end
+
   
